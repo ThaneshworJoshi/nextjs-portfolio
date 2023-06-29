942b2085326afa05c01b7e265608ecac5048aa70
Thaneshwor Joshi <thaneshwor10@gmail.com>
2023-06-29 09:54:11 +0545

    add: Blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,98 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./page.module.css";
 
 const BlogDetail = () => {
-  return <div>BlogDetail</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            ullam quia eveniet, ex aperiam suscipit nobis iusto ab maiores
+            quisquam eum voluptatibus quam earum illo. Hic facere animi in
+            eligendi!
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://plus.unsplash.com/premium_photo-1682464767558-448de834ed4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>JOHN</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://plus.unsplash.com/premium_photo-1682464767558-448de834ed4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+          molestias consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis at, labore atque unde fugit esse molestias alias quo
+          nisi.ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+          molestias consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis at, labore atque unde fugit esse molestias alias quo
+          nisi.
+          <br/>
+          <br/>
+
+          ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+          molestias consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis at, labore atque unde fugit esse molestias alias quo
+          nisi.ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+          molestias consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis at, labore atque unde fugit esse molestias alias quo
+          nisi.ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+          molestias consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis at, labore atque unde fugit esse molestias alias quo
+          nisi.
+          ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+          molestias consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis at, labore atque unde fugit esse molestias alias quo
+          nisi.
+          ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+          molestias consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis at, labore atque unde fugit esse molestias alias quo
+          nisi.ipsum 
+          <br/>
+          <br/>
+
+          dolor sit amet consectetur adipisicing elit. Quaerat,
+          molestias consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis at, labore atque unde fugit esse molestias alias quo
+          nisi. as consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis aas consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis a
+          as consectetur! Debitis, non aut ex, minima nisi ullam qui
+          officiis magnam omnis unde quia! Eum rerum voluptates, aspernatur
+          corporis, debitis a
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogDetail;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,76 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import styles from "./page.module.css";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className={styles.container}>
+      <Link href="/blog/testId" className={styles.blogContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.unsplash.com/photo-1674574124345-02c525664b65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint explicabo magnam aperiam totam provident itaque quod fuga ipsam incidunt ipsum! Illo animi amet corrupti porro magni? Architecto laborum fugiat quasi quod consequuntur, odit sint sapiente eligendi corrupti modi nobis dolor!</p>
+        </div>
+      </Link>
+
+      <Link href="/blog/testId" className={styles.blogContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.unsplash.com/photo-1687463221023-02f259da7d77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Islld ipsum dolor sit amet sdfsf.</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur, adipisicing sit amet consectetur, adipisicing sit amet consectetur, adipisicing sit amet consectetur, adipisicing sit amet consectetur, adipisicing elit. Sint explicabo magnam aperiam totam provident itaque quod fuga ipsam incidunt ipsum! Illo animi amet corrupti porro magni? Architecto laborum fugiat quasi quod consequuntur, odit sint sapiente eligendi corrupti modi nobis dolor!</p>
+        </div>
+      </Link>
+
+      <Link href="/blog/testId" className={styles.blogContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.unsplash.com/photo-1687368438644-3ba4c870a26c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzN3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint explicabo magnam aperiam totam provident itaque quod fuga ipsam incidunt ipsum! Illo animi amet corrupti porro magni? Architecto laborum fugiat quasi quod consequuntur, odit sint sapiente eligendi corrupti modi nobis dolor!</p>
+        </div>
+      </Link>
+
+      <Link href="/blog/testId" className={styles.blogContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://plus.unsplash.com/premium_photo-1686904396346-fc96d53fe368?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint explicabo magnam aperiam totam provident itaque quod fuga ipsam incidunt ipsum! Illo animi amet corrupti porro magni? Architecto laborum fugiat quasi quod consequuntur, odit sint sapiente eligendi corrupti modi nobis dolor!</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
